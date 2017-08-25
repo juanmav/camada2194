d180a320f686c892a63d94be051739022d294b48
Juan Manuel Vicente <juanma.v82@gmail.com>
2017-08-25 11:46:10 -0300

    componentes y estados en reactjs

--- clase4/src/componets/postlist.js
+++ clase4/src/componets/postlist.js
@@ -3,19 +3,47 @@ import Post from './post';
 
 export default class PostList extends React.Component {
 
-    constructor(props){
-        super(props);
+    constructor(){
+        super();
+        this.state = {
+            posts: []
+        }
     }
+
+    componentWillMount(){
+        fetch('http://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(posts => {
+                this.setState({posts: posts})
+            })
+    }
+
+    borrarPost = (id) => {
+        console.log('borrameeeeee: ' + id);
+        let updatedList = this.state.posts.filter(p => p.id != id);
+        this.setState({ posts: updatedList});
+    };
 
     render(){
         return (
             <div>
                 {
-                    this.props.posts.map((p, index) => {
+
+                    this.state.posts.length ?
+                    this.state.posts.map((p, index) => {
                         return (
-                            <Post key={index} title={p.title} body={p.body}/>
+                            <Post
+                                key={index}
+                                id={p.id}
+                                title={p.title}
+                                body={p.body}
+                                initialCounter={10}
+                                borrarFn={this.borrarPost}
+                            />
                         )
                     })
+                        :
+                        'Cargando.......!!!'
                 }
             </div>
         )
